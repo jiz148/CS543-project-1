9cd396d61452e11c30ee85258d12e1b68919dcc9
Jinchi Zhang <jizjiz148148@gmail.com>
2021-11-01 17:14:11 -0400

    modified consumer

--- backend/kafka/consumer.py
+++ backend/kafka/consumer.py
@@ -1,10 +1,15 @@
 #consumer
 from kafka import KafkaConsumer
+import json
 
-consumer = KafkaConsumer('data', bootstrap_servers=['localhost:9092'],value_deserializer=lambda m: json.loads(m.decode('ascii')))
+consumer = KafkaConsumer('data', bootstrap_servers=['localhost:9092'],value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+#value_deserializer=lambda m: json.loads(m.decode('ascii'))
 
+print(consumer)
 for message in consumer:
+    print("\n hello")
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key,message.value))
+    
 
 
     
